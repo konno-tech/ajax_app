d639a3740fe8da5dfbe0205fe75df6c0442013a8
konno-tech <kontc723@gmail.com>
2020-10-19 16:10:38 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,14 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: "DESC")
   end
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    # 以下の2行はデータ形式でのレスポンスではなくHTML形式のレスポンス。
+    # 非同期通信は画面のリロードを行わずに行われるため、これに合わせた処理に修正。
+    # Post.create(content: params[:content])
+    # redirect_to action: :index
+    # 未読の情報を保存
+    post = Post.create(content: params[:content], checked: false)
+    # レスポンスをjsonに変更
+    render json:{ post: post }
   end
   def checked
     post = Post.find(params[:id])
